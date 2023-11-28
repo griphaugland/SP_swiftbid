a80c2336b05fdb55421c00b58de0e78365fca814
griphaugland <griphaugland@gmail.com>
2023-11-28 18:38:17 +0100

    Added functional log-in and register pages

---
+++ js/modules/login.mjs
@@ -0,0 +1,44 @@
+import { startLoading, stopLoading } from "./loader.mjs";
+
+const button = document.getElementById("login");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  login();
+});
+
+async function login() {
+  const url = "https://api.noroff.dev/api/v1/auction/auth/login";
+  const email = document.querySelector("#input-email");
+  const password = document.querySelector("#input-password");
+  const response = document.querySelector("#response");
+  startLoading("login");
+  if (email.value === "" || password.value === "") {
+    stopLoading("login");
+    response.innerHTML = `<img height="20" width="20" alt="error icon" src="../../media/circle-exclamation-solid.svg"><p class="response-text" > Email or password is empty</p>`;
+  } else {
+    const user = {
+      email: email.value,
+      password: password.value,
+    };
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+    localStorage.setItem("profile", JSON.stringify(data));
+    console.log(data);
+    if (data.errors) {
+      stopLoading("login");
+      response.innerHTML = `<img height="20" width="20" alt="error icon" src="../../media/circle-exclamation-solid.svg"><p class="response-text" > Email or password is incorrect</p>`;
+    } else {
+      response.innerHTML = `<img height="20" width="20" alt="sucess icon" src="../../media/circle-check-regular.svg"> <p class="response-text" >Logging in</p>`;
+      setTimeout(() => {
+        stopLoading("login");
+        window.location.href = "../../feed";
+      }, 1000);
+    }
+  }
+}

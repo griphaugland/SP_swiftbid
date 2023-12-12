8cab75b285d0928d8e78d48c16c8806597157f21
griphaugland <griphaugland@gmail.com>
2023-12-13 00:48:59 +0100

    styling changes, and getting ready to start on profile

--- js/modules/isLoggedIn.mjs
+++
@@ -1,9 +0,0 @@
-export default function isLoggedIn() {
-  let isLoggedIn;
-  if (!localStorage.getItem("profile")) {
-    isLoggedIn = false;
-  } else {
-    isLoggedIn = true;
-  }
-  return isLoggedIn;
-}
--- js/modules/logout.mjs
+++
@@ -1,6 +0,0 @@
-export default function logout() {
-  localStorage.clear();
-  setTimeout(() => {
-    window.location.href = "../auth/login";
-  }, 500);
-}
